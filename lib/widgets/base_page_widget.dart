80452f65d37951d5f649dc54c3ea5285040be4ec
Max Marschhauser <max.marschhauser@gmail.com>
2024-06-15 12:12:19 +0200

    added menu

--- lib/widgets/base_page_widget.dart
+++ lib/widgets/base_page_widget.dart
@@ -5,7 +5,7 @@ import 'package:go_router/go_router.dart';
 
 import 'container.dart';
 
-class EDKBasePageWidget extends StatelessWidget {
+class EDKBasePageWidget extends StatefulWidget {
   final Widget child;
   final String image;
   final String titleText;
@@ -18,97 +18,158 @@ class EDKBasePageWidget extends StatelessWidget {
   });
 
   @override
+  State<EDKBasePageWidget> createState() => _EDKBasePageWidgetState();
+}
+
+class _EDKBasePageWidgetState extends State<EDKBasePageWidget> {
+  bool _showMenu = false;
+
+  @override
   Widget build(BuildContext context) {
     return SelectableRegion(
       focusNode: FocusNode(),
       selectionControls: desktopTextSelectionControls,
-      child: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          bottom: PreferredSize(
-              preferredSize: const Size(0, 1),
-              child: Container(
-                height: 1,
-                color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
-              )),
-          title: MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: () {
-                context.go(EDKHomeRoute.home.fullPath);
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  "Expert DK - procjene nekretnina",
-                  style: Theme.of(context).textTheme.titleLarge,
+      child: Stack(
+        children: [
+          Scaffold(
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              bottom: PreferredSize(
+                preferredSize: const Size(0, 1),
+                child: Container(
+                  height: 1,
+                  color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
                 ),
+              ),
+              centerTitle: false,
+              title: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: () {
+                    context.go(EDKHomeRoute.home.fullPath);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      "Expert DK - procjene nekretnina",
+                      style: MediaQuery.of(context).size.width > 1000 ? Theme.of(context).textTheme.titleLarge : Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                ),
+              ),
+              leading: InkWell(
+                hoverColor: Theme.of(context).colorScheme.secondary,
+                splashColor: Theme.of(context).colorScheme.primary,
+                hoverDuration: const Duration(milliseconds: 250),
+                onTap: () {
+                  context.go(EDKHomeRoute.home.fullPath);
+                },
+                child: Container(
+                  color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+                  padding: const EdgeInsets.all(8),
+                  child: Image.asset(
+                    EDKImages.shared.logo,
+                  ),
+                ),
+              ),
+              actions: [
+                GestureDetector(
+                  onTap: () {
+                    _showMenu = !_showMenu;
+                    setState(() {});
+                  },
+                  child: Container(
+                    color: Colors.transparent,
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: Icon(
+                      _showMenu ? Icons.close : Icons.menu,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset(
+                    widget.image,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: EDKContainer.primaryContainer(
+                      text: widget.titleText,
+                      textStyle: Theme.of(context).textTheme.headlineLarge!,
+                      context: context,
+                    ),
+                  ),
+                  Center(
+                    child: widget.child,
+                  ),
+                  _footerContainer(context),
+                ],
               ),
             ),
           ),
-          leading: InkWell(
-            hoverColor: Theme.of(context).colorScheme.secondary,
-            splashColor: Theme.of(context).colorScheme.primary,
-            hoverDuration: const Duration(milliseconds: 250),
-            onTap: () {
-              context.go(EDKHomeRoute.home.fullPath);
-            },
+          Positioned(
+            top: 57,
+            right: 0,
             child: Container(
-              color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
-              padding: const EdgeInsets.all(8),
-              child: Image.asset(
-                EDKImages.shared.logo,
+              color: Colors.white,
+              width: 200,
+              height: _showMenu ? 135 : 0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _menuItem(
+                      text: "Početna stranica",
+                      function: () {
+                        context.go(EDKHomeRoute.home.fullPath);
+                      }),
+                  _menuItem(
+                      text: "Kvalifikacije",
+                      function: () {
+                        context.go(EDKHomeRoute.kvalifikacije.fullPath);
+                      }),
+                  _menuItem(
+                      text: "Kontakt",
+                      function: () {
+                        context.go(EDKHomeRoute.kontakt.fullPath);
+                      }),
+                ],
               ),
             ),
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                context.go(EDKHomeRoute.home.fullPath);
-              },
-              child: Text("Početna stranica"),
-            ),
-            const SizedBox(width: 8),
-            ElevatedButton(
-              onPressed: () {
-                context.go(EDKHomeRoute.kvalifikacije.fullPath);
-              },
-              child: Text("Kvalifikacije"),
-            ),
-            const SizedBox(width: 8),
-            ElevatedButton(
-              onPressed: () {
-                context.go(EDKHomeRoute.kontakt.fullPath);
-              },
-              child: Text("Kontakt"),
-            ),
-            const SizedBox(width: 8),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                image,
-                height: MediaQuery.of(context).size.height * 0.4,
-                width: double.infinity,
-                fit: BoxFit.cover,
-                alignment: Alignment.center,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: EDKContainer.primaryContainer(
-                  text: titleText,
-                  textStyle: Theme.of(context).textTheme.headlineLarge!,
-                  context: context,
-                ),
-              ),
-              Center(
-                child: child,
-              ),
-              _footerContainer(context),
-            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _menuItem({required String text, required Function function}) {
+    return Material(
+      child: InkWell(
+        hoverColor: Theme.of(context).colorScheme.secondary,
+        splashColor: Theme.of(context).colorScheme.primary,
+        hoverDuration: const Duration(milliseconds: 250),
+        onTap: () {
+          function();
+          _showMenu = false;
+          setState(() {});
+        },
+        child: Container(
+          decoration: BoxDecoration(color: Theme.of(context).colorScheme.secondary.withOpacity(0.5), border: Border(bottom: BorderSide(color: Theme.of(context).colorScheme.primary))),
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.primary),
+            textAlign: TextAlign.end,
           ),
         ),
       ),
